e5e73d567a2782687d18fa9de1a47e2f425cdef7
parksol <thfdl0317@naver.com>
2022-02-15 23:25:32 +0900

    사용자 목록 조회를 위한 API구현

--- restful-web-service/src/main/java/com/sole/restfulwebservice/User/UserDaoService.java
+++ restful-web-service/src/main/java/com/sole/restfulwebservice/User/UserDaoService.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class UserDaoService {
 	private static List<User> users = new ArrayList<User>();
 	
@@ -15,8 +19,26 @@ public class UserDaoService {
 //		users.add(new User(id:1, name:"Kenneth", new Date()));
 //		users.add(new User(id:2, name:"Alice", new Date()));
 //		users.add(new User(id:3, name:"Elena", new Date()));
+		
+		
 	}
 	
+	public static List<User> getUsers() {
+		return users;
+	}
+
+	public static void setUsers(List<User> users) {
+		UserDaoService.users = users;
+	}
+
+	public static int getUsersCount() {
+		return usersCount;
+	}
+
+	public static void setUsersCount(int usersCount) {
+		UserDaoService.usersCount = usersCount;
+	}
+
 	public List<User> findAll(){
 		return users;
 	}
