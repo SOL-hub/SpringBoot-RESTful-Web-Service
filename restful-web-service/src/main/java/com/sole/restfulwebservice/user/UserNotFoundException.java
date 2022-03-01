79b86d1ccbee6a3ff506ba41d36e6e2b33e9c5e0
parksol <thfdl0317@naver.com>
2022-03-02 05:40:57 +0900

    Http status Code 제어를 위한 Exception Handing

--- restful-web-service/src/main/java/com/sole/restfulwebservice/user/UserNotFoundException.java
+++ restful-web-service/src/main/java/com/sole/restfulwebservice/user/UserNotFoundException.java
@@ -1,11 +1,15 @@
-package com.sole.restfulwebservice.User;
+package com.sole.restfulwebservice.user;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+//200은 ok
+//400은 client가 뭔가문제
+//500은 서버 내부에 문제
+
+@ResponseStatus(HttpStatus.NOT_FOUND)//이제 이 예외클래스는 404로 postman에 실행해보면 status값은 404로 떠야 정상작동한 것이다.
 public class UserNotFoundException extends RuntimeException {
 	public UserNotFoundException(String message) {
-		super(message);
+		super(message); //생성자는 부모클래스에서 전달받은 메세지로
 	}
 }
